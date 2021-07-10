5cf47122a19f4cfb169108e46169444566d69af4
nguyentri <nguyentri2815@gmail.com>
2021-07-10 16:42:45 +0700

    fix

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 const guessNumber = Math.round(Math.random() * 100);
+
 document.querySelector('.number-random').innerHTML = guessNumber;
+
 let count = 0;
+
 const CheckNumber = () => {
   count++;
   let valueInput = +document.querySelector('.form-control').value;
-  
   let node = document.createElement("LI");
   node.classList.add('list-group-item');
   let textnode = document.createTextNode(`Lần ${count}: ${valueInput}`);
@@ -13,14 +15,11 @@ const CheckNumber = () => {
 
   if (valueInput > guessNumber) {
     document.querySelector('.alert-mess').innerHTML = 'sô nhập vào lớn hơn số cần tìm ';
-  } else if (valueInput < guessNumber) {
+  }else if (valueInput < guessNumber) {
     document.querySelector('.alert-mess').innerHTML = 'sô nhập vào nhỏ hơn số cần tìm ';
-  } else if (valueInput = guessNumber) {
+  }else{
     document.querySelector('.alert-mess').innerHTML = 'Chúc mứng bạn đã đoán đúng số ';
     document.querySelector('.list-group').classList.remove('d-none');
-  } else {
-    document.querySelector('.alert-mess').innerHTML = 'sô nhập vào khống đúng định dạng';
   }
-
 
 }
